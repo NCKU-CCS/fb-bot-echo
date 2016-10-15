3ac0967d7fd79a522092fcf28c071fc6ad7331b4
Frank Yang <yangpoan@gmail.com>
2016-10-15 11:48:03 +0800

    fix for fb

--- fbecho/pages/views.py
+++ fbecho/pages/views.py
@@ -3,4 +3,5 @@ from django.views import View
 
 class HomePage(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("純粹測試FB bot功能，並不會紀錄任何使用者資訊：）",status=200)
+        html_str = "<html><head><title>Test For Msg</title><body>純粹測試FB bot功能，並不會紀錄任何使用者資訊：）</body></head></html>"
+        return HttpResponse(html_str,status=200)
